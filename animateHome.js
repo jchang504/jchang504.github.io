fbb9f4071df3b4a54c6819531a17093200a92021
Jemmin Chang <jemminchang@cmu.edu>
2014-06-27 23:33:38 -0400

    Links and section navigation added.
    The links to sections are now enabled and looking good (still no subsections
    for Projects and More, as the content hasn't been added yet). Dummy content
    in each section for now. Still need to address the backed-up queueing issue
    with the slide animations (for the picture window, menu option-lists, and
    section navigation(? almost passable w/o)). Then, section content needs to
    be filled in, and links to subsections added (this may screw up animations).

--- animateHome.js
+++ animateHome.js
@@ -1,21 +1,24 @@
 $(document).ready(function() {
   // Hides the "no JS?" message
   $('#nojs').css('display', 'none');
+  pictureWindowUp = true;
   // Sliding picture window
   $('.menu-item').hover(function() {
-    switch($(this).attr('id')) {
-      case 'about':
-        $('#picwindow').animate({backgroundPosition:'-350px'}, 100);
-        break;
-      case 'projects':
-        $('#picwindow').animate({backgroundPosition:'-700px'}, 100);
-        break;
-      case 'more':
-        $('#picwindow').animate({backgroundPosition:'-1050px'}, 100);
-        break;
-      case 'contact':
-        $('#picwindow').animate({backgroundPosition:'-1400px'}, 100);
-        break;
+    if (pictureWindowUp) {
+      switch($(this).attr('id')) {
+        case 'about-link':
+          $('#picwindow').animate({backgroundPosition:'-350px'}, 100);
+          break;
+        case 'nav-projects':
+          $('#picwindow').animate({backgroundPosition:'-700px'}, 100);
+          break;
+        case 'nav-more':
+          $('#picwindow').animate({backgroundPosition:'-1050px'}, 100);
+          break;
+        case 'contact-link':
+          $('#picwindow').animate({backgroundPosition:'-1400px'}, 100);
+          break;
+      }
     }
   });
   // Option menus for Projects and More
@@ -23,5 +26,23 @@ $(document).ready(function() {
     $(this).children('.options-list').slideDown('fast');
   }, function() {
     $(this).children('.options-list').slideUp('fast');
+  });
+  // Navigation for sections
+  currentSectionId = "";
+  $('.menu-link').click(function() {
+    clickedSectionId = '#' + $(this).attr('id').replace('link', 'section');
+    if (clickedSectionId !== currentSectionId) { 
+      // don't slide for initial section choice; it looks weird
+      if (pictureWindowUp) {
+        $('#picwindow').hide();
+        pictureWindowUp = false;
+        $(clickedSectionId).show();
+      }
+      else { // after that, slideUp/Down combination gives nice layer effect
+        $('section').slideUp('fast');
+        $(clickedSectionId).slideDown('fast');
+        currentSectionId = clickedSectionId;
+      }
+    }
   });
 });
